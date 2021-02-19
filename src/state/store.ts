3758f777303f27901130ec127c4efa85e43032c9
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-18 22:15:29 -0300

    feat: Adicionado a tipagem da Store geral

--- src/state/store.ts
+++ src/state/store.ts
@@ -4,10 +4,11 @@ import {
   getDefaultMiddleware,
 } from "@reduxjs/toolkit";
 
-import { companyReducer } from "./company/slicer";
+import { companyReducer as company } from "./company/slicer";
 
-const reducer = combineReducers({ companyReducer });
+const reducer = combineReducers({ company });
 const middleware = [...getDefaultMiddleware()];
 const store = configureStore({ reducer, middleware });
 
+export type StoreTypes = ReturnType<typeof reducer>;
 export default store;
