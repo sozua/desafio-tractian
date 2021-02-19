8d5c1df710d53b82c9b8a2f9f835f1c896a79768
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-18 22:21:02 -0300

    feat: Criado arquivo de referência para tipagens da API

---
+++ src/utils/types.ts
@@ -0,0 +1,13 @@
+export type UnityProps = {
+  id?: number;
+  name?: string;
+  companyId?: number;
+};
+
+export type UserProps = {
+  id?: number;
+  email?: string;
+  name?: string;
+  unitId?: number;
+  companyId?: number;
+};
