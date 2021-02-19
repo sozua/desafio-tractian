c66cd42efae5dd0488c66f886bbffef04eab1d24
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-18 22:14:32 -0300

    refactor: Melhorada as tipagens do CompanySlicer

--- src/state/company/slicer.ts
+++ src/state/company/slicer.ts
@@ -1,30 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UnityProps, UserProps } from "../../utils/types";
+
+type SliceProps = {
+  name: string;
+  unities: UnityProps[];
+  users: UserProps[];
+  actualUser: number;
+};
+
+const initialState: SliceProps = {
+  name: "",
+  unities: [],
+  users: [],
+  actualUser: -1,
+};
 
 const companySlicer = createSlice({
   name: "company",
-  initialState: {
-    name: "",
-    unities: [],
-    users: [],
-  },
+  initialState,
   reducers: {
-    changeName(state, action) {
+    changeName(state, action: PayloadAction<string>) {
       state.name = action.payload;
     },
     clearName(state) {
       state.name = "";
     },
-    changeUnities(state, action) {
+    changeUnities(state, action: PayloadAction<[]>) {
       state.unities = action.payload;
     },
     clearUnities(state) {
       state.unities = [];
     },
-    changeUsers(state, action) {
+    changeUsers(state, action: PayloadAction<[]>) {
       state.users = action.payload;
     },
     clearUsers(state) {
       state.users = [];
+    },
+    changeActualUser(state, action: PayloadAction<number>) {
+      state.actualUser = action.payload;
+    },
+    clearActualUser(state) {
+      state.actualUser = -1;
     },
   },
 });
@@ -36,6 +53,8 @@ export const {
   clearUnities,
   changeUsers,
   clearUsers,
+  changeActualUser,
+  clearActualUser,
 } = companySlicer.actions;
 
 export const companyReducer = companySlicer.reducer;
