dd0484643f9897530acc4a52586e173a8d4eda05
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-19 15:34:39 -0300

    feat: Importado o componente AssetsCard à Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Col, Row } from "antd";
+
+import AssetsCard from "../../components/AssetsCard/";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <Row gutter={24} style={{ marginTop: 32 }}>
+      <Col xs={24} lg={12}>
+        <AssetsCard />
+      </Col>
+    </Row>
+  );
 };
 
 export default Home;
