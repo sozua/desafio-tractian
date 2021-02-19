f8bb7872586009131e6a0f5f43121012eaa22094
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-19 15:35:33 -0300

    feat: Implementado rotas da Home

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,15 @@ import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import store from "./state/store";
 
+import { ConfigProvider } from "antd";
+import ptBR from "antd/lib/locale/pt_BR";
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConfigProvider locale={ptBR}>
+        <App />
+      </ConfigProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
