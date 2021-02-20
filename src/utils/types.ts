af49ca74cdf1c1dc8c197d03cda610b1c2252a72
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-20 17:08:21 -0300

    feat: Adicionada a tipagem de Assets no arquivo types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,26 @@
+export type AssetProps = {
+  id: number;
+  name: string;
+  model: string;
+  sensors: string[];
+  image: string;
+  healthscore?: number;
+  status?: string;
+  specifications: {
+    maxTemp?: number;
+    power?: number;
+    rpm?: number;
+  };
+  metrics: {
+    totalCollectsUptime: number;
+    totalUptime: number;
+    lastUptimeAt: string;
+  };
+  unitId?: number;
+  unitName?: string;
+  companyId?: number;
+};
+
 export type UnityProps = {
   id?: number;
   name?: string;
