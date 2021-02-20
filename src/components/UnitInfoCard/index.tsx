a4dc87230cc3e9a60276c7edd352e945249a1527
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-20 01:21:21 -0300

    feat: Criado componente UnitInfoCard

---
+++ src/components/UnitInfoCard/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import useUserLogged from "../../hooks/useUserLogged";
+import { findUsers, findAssets, findSingleUnity } from "../../utils/api";
+import StatsCard from "../StatsCard";
+
+const initialDataState = [
+  { title: "Nome", value: "" },
+  { title: "Ativos em sua unidade", value: 0 },
+  { title: "Funcionários em sua unidade", value: 0 },
+];
+
+const UnitInfoCard = () => {
+  const { actualUser, isLogged } = useUserLogged();
+  const [data, setData] = useState(initialDataState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData(companyId: number, unitId: number) {
+      try {
+        setLoading(true);
+        const unitName = (await findSingleUnity(unitId)).name;
+        const usersInUnit = await findUsers(companyId, unitId)();
+        const assetsInUnit = await findAssets(companyId, unitId)();
+        setData([
+          { title: "Nome", value: unitName },
+          { title: "Ativos em sua unidade", value: assetsInUnit.length },
+          { title: "Funcionários em sua unidade", value: usersInUnit.length },
+        ]);
+      } catch (err) {
+        setData(initialDataState);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (isLogged && actualUser.companyId && actualUser.unitId) {
+      fetchData(actualUser.companyId, actualUser.unitId);
+    }
+  }, [isLogged, actualUser.companyId, actualUser.unitId]);
+
+  return (
+    <StatsCard
+      title="Informações da sua unidade"
+      loading={loading}
+      stats={data}
+    />
+  );
+};
+
+export default UnitInfoCard;
