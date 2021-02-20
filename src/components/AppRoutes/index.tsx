faef64ecdf53130f00c6eef02ab13744a2883a20
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-20 17:15:33 -0300

    feat: Criada a página SingleAsset

--- src/components/AppRoutes/index.tsx
+++ src/components/AppRoutes/index.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "../../pages/Home";
 import AssetsPage from "../../pages/Assets";
+import SingleAsset from "../../pages/SingleAsset";
 
 const GlobalRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="ativos" element={<AssetsPage />} />
+      <Route path="ativos/:id" element={<SingleAsset />} />
     </Routes>
   );
 };
---
+++ src/pages/SingleAsset/index.tsx
@@ -0,0 +1,52 @@
+import { EditOutlined } from "@ant-design/icons";
+import { Button, Col, Row } from "antd";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import AppPageHeader from "../../components/PageHeader";
+import MetricsCard from "../../components/MetricsCards";
+import SpecificationsCard from "../../components/SpecificationsCard";
+import useLoadFromApi from "../../hooks/useLoadFromApi";
+import { findSingleAsset, findSingleUnity } from "../../utils/api";
+import AssetBasicInfosCard from "../../components/AssetBasicInfosCard";
+
+const SingleAsset = () => {
+  const { id } = useParams();
+  const { data, loading, apiRequest, setData } = useLoadFromApi();
+
+  useEffect(() => {
+    async function getUnitName() {
+      const unitName = (await findSingleUnity(data.unitId)).name || "";
+      setData((oldData: any) => ({ ...oldData, unitName }));
+    }
+    apiRequest(findSingleAsset(id));
+    getUnitName();
+  }, [apiRequest, id, setData, data.unitId]);
+
+  return (
+    <Row gutter={24} style={{ marginTop: 32 }}>
+      <AppPageHeader
+        title={data.name}
+        loading={loading}
+        extra={[
+          <Button key="1" type="primary">
+            <EditOutlined />
+            Editar
+          </Button>,
+        ]}
+      />
+      <Col xs={24} lg={12}>
+        <AssetBasicInfosCard
+          title="Informações básicas"
+          loading={loading}
+          data={data}
+        />
+      </Col>
+      <Col xs={24} lg={12}>
+        <SpecificationsCard assetData={data} loading={loading} />
+        <MetricsCard assetData={data} loading={loading} />
+      </Col>
+    </Row>
+  );
+};
+
+export default SingleAsset;
