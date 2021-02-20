6664f9456c7195eac227e98902e639b75196d1f3
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-19 21:51:15 -0300

    feat: Adicionado o UsersCard à página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,16 @@
 import { Col, Row } from "antd";
 
 import AssetsCard from "../../components/AssetsCard/";
+import UsersCard from "../../components/UsersCard";
 
 const Home = () => {
   return (
     <Row gutter={24} style={{ marginTop: 32 }}>
       <Col xs={24} lg={12}>
         <AssetsCard />
+      </Col>
+      <Col xs={24} lg={12}>
+        <UsersCard />
       </Col>
     </Row>
   );
