157c71a98a50124a5369f997a06b849ff3d56e6b
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-19 21:44:30 -0300

    feat: Criado o componente AssetsInDowntimeCard

---
+++ src/components/AssetsInDowntimeCard/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import useUserLogged from "../../hooks/useUserLogged";
+import { findInDowntimeAssets } from "../../utils/api";
+
+import { RadioChangeEvent } from "antd";
+import useLoadFromApi from "../../hooks/useLoadFromApi";
+import ListCard from "../ListCard";
+import assetsColumnsScheme from "./columnsScheme";
+
+const AssetsInDowntimeCard = () => {
+  const { actualUser, isLogged } = useUserLogged();
+  const { data, loading, apiRequest } = useLoadFromApi();
+
+  useEffect(() => {
+    if (isLogged && actualUser.id && actualUser.id >= 0 && actualUser.companyId)
+      apiRequest(findInDowntimeAssets(actualUser.companyId, actualUser.id));
+  }, [actualUser, isLogged, apiRequest]);
+
+  function onChange(event: RadioChangeEvent) {
+    if (actualUser.companyId)
+      switch (event.target.value) {
+        case "userUnit":
+          apiRequest(findInDowntimeAssets(actualUser.companyId, actualUser.id));
+          break;
+        default:
+          apiRequest(findInDowntimeAssets(actualUser.companyId));
+      }
+  }
+
+  const radioButtons = [
+    { title: "Ativos da sua unidade", value: "userUnit" },
+    { title: "Todos os ativos", value: "all" },
+  ];
+
+  return (
+    <ListCard
+      title="Ativos em parada"
+      radioButtons={radioButtons}
+      onRadioChange={onChange}
+      columns={assetsColumnsScheme}
+      dataSource={data}
+      loading={loading}
+    />
+  );
+};
+
+export default AssetsInDowntimeCard;
