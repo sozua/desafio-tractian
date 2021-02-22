83d75bfbff88af43e344165295288a9c31afd8ea
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-21 21:26:31 -0300

    feat: Adicionado o componente AssetsCard na page Assets

--- src/pages/Assets/index.tsx
+++ src/pages/Assets/index.tsx
@@ -3,12 +3,14 @@ import { Col, Row } from "antd";
 import AssetsInDowntimeCard from "../../components/AssetsInDowntimeCard/";
 import AssetsInAlertCard from "../../components/AssetsInAlertCard/";
 import MetricsComparationCard from "../../components/MetricsComparationCard";
+import AssetsCard from "../../components/AssetsCard";
 
 const AssetsPage = () => {
   return (
     <Row gutter={24} style={{ marginTop: 32 }}>
       <Col xs={24} lg={12}>
         <MetricsComparationCard />
+        <AssetsCard />
       </Col>
       <Col xs={24} lg={12}>
         <AssetsInDowntimeCard />
