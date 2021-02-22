0459760693f8608abc4819c542d6cd29310d1561
Diogo de Souza <diogosrpriv@gmail.com>
2021-02-22 15:21:13 -0300

    refactor: Agora o UnitInfoCard utiliza os dados servidos globalmente

--- src/components/UnitInfoCard/index.tsx
+++ src/components/UnitInfoCard/index.tsx
@@ -10,33 +10,34 @@ const initialDataState = [
 ];
 
 const UnitInfoCard = () => {
-  const { actualUser, isLogged } = useUserLogged();
+  const { actualUser, isLogged, company } = useUserLogged();
   const [data, setData] = useState(initialDataState);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData(companyId: number, unitId: number) {
-      try {
-        setLoading(true);
-        const unitName = (await findSingleUnit(unitId)).name;
-        const usersInUnit = await findUsers(companyId, unitId)();
-        const assetsInUnit = await findAssets(companyId, unitId)();
-        setData([
-          { title: "Nome", value: unitName },
-          { title: "Ativos em sua unidade", value: assetsInUnit.length },
-          { title: "Funcionários em sua unidade", value: usersInUnit.length },
-        ]);
-      } catch (err) {
-        setData(initialDataState);
-      } finally {
-        setLoading(false);
-      }
-    }
+    if (isLogged && actualUser.unitId) {
+      const userUnit = company.units[actualUser.unitId - 1];
+      const unitName = userUnit.name || "";
+      const usersInUnit = company.users.filter(
+        (user) => user.unitId === actualUser.unitId
+      ).length;
+      const assetsInUnit = company.assets.length;
 
-    if (isLogged && actualUser.companyId && actualUser.unitId) {
-      fetchData(actualUser.companyId, actualUser.unitId);
+      setData([
+        { title: "Nome", value: unitName },
+        { title: "Ativos em sua unidade", value: assetsInUnit },
+        { title: "Funcionários em sua unidade", value: usersInUnit },
+      ]);
+      setLoading(false);
     }
-  }, [isLogged, actualUser.companyId, actualUser.unitId]);
+  }, [
+    isLogged,
+    actualUser.companyId,
+    actualUser.unitId,
+    company.units,
+    company.assets.length,
+    company.users,
+  ]);
 
   return (
     <StatsCard
